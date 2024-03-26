1522e516b6c6cf526e0e88110507871fc645f02b
trungnd1409 <trungnguyenb0k30@gmail.com>
2024-03-26 15:02:24 +0100

    filering functionality

---
+++ webapp/controller/ReferenceList.controller.js
@@ -0,0 +1,39 @@
+sap.ui.define(["./BaseController","sap/ui/model/Filter","sap/ui/model/FilterOperator"], function (BaseController,Filter,FilterOperator) {
+	"use strict";
+
+	return BaseController.extend("reference.management.app.controller.ReferenceList", {
+		onFilterReference(oEvent){
+            // create some filters to filter data
+            const aFilter = [];
+            // get the query that is provided by the search field
+            const sQuery = oEvent.getParameter("query");
+            if(sQuery){
+                console.log(sQuery)
+                const combinedFilter = new Filter({
+                    filters: [
+                        new Filter("type", FilterOperator.Contains, sQuery),
+                        new Filter("title", FilterOperator.Contains, sQuery),
+                        new Filter("publication_date", FilterOperator.Contains, sQuery),
+                    ],
+                    and: false
+                });
+                aFilter.push(combinedFilter);
+            }
+            // get the reference to the table
+            const oTable = this.byId("literature");
+            //get the data from the table
+            const oBinding = oTable.getBinding("items");
+            oBinding.filter(aFilter);
+            console.log("filtering")
+        }
+        /*onPressCustomer(oEvent){
+            // oItem refers to the clikced line, e.g., one customer
+            var oItem = oEvent.getSource();
+            this.getRouter().navTo("detail",{
+                customerId: oItem.getBindingContext().getPath().substring('/customers/'.length)
+            })
+            //this.getRouter().navTo("detail");
+
+        }*/
+	});
+});
